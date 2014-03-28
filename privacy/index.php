99b7358a2a251c1396d08e505679cf1b35bca739
David Delmar <ddelmar@where.com>
2014-03-28 15:56:43 -0400

    privacy policy updates

--- privacy/index.php
+++ privacy/index.php
@@ -1,51 +1,65 @@
 <?php include('../includes/head.php'); ?>
 
-
+<style>
+h2 { padding-top: 90px; }
+</style>
 <div class="onecolumn">
 <h1>Privacy Policy</h1>
 <div id="wrapper">
 	<div id="container">
+	<p>User Privacy Notice</p><p>Last Update: 3/10/14 
+</p><p>Your Privacy Rights </p><p>Start Tank is a PayPal an incubator with free co-working space for the next generation of startups.  This User Privacy Notice describes your privacy rights regarding our collection, use, storage, sharing and protection of your personal information. It applies to the website www.starttank.com and all related sites, applications, services and tools regardless of how you access or use them. 
+</p><p>You accept this User Privacy Notice when you sign up for, access, or use our products, services, content, features, technologies or functions offered on our website and all related sites, applications, and services (collectively "Start Tank Services"). We may amend the User Privacy Notice at any time by posting a revised version on our website. The revised version will be effective at the time we post it. In addition, if the revised version includes a substantial change, we will provide you with 30 days’ prior notice by posting notice of the change on the "Policy Updates" page of our website. </p>
 	
+	<ul>
+		<li><a href="#one">How we collect information about you</a></li>
+		<li><a href="#two">How we use Cookies </a></li>
+		<li><a href="#three">How we protect and store personal information</a></li>
+		<li><a href="#four">How we use the personal information we collect</a></li>
+		<li><a href="#five">Marketing</a></li>
+		<li><a href="#six">How we share personal information with other parties </a></li>
+		<li><a href="#seven">How you can access or change your personal information</a></li>
+		<li><a href="#eight">How you can contact us about privacy questions </a></li>
+	</ul>
+	
+	
+<h2 id="one">How We Collect Information about You </h2>
 
-								<p><strong>Scope</strong></p>
-								<p>Start Tank (“Start Tank”) is a division of PayPal and part of the eBay Inc. corporate family.  This Privacy Statement describes Start Tank’s collection, storage, use and protection of personal information associated with ad requests and relating to its advertising network.   The Statement applies to Start Tank’s mobile advertising products and services and our company website located at <a href="https://advertising.paypal.com">https://advertising.paypal.com.</a> We reserve the right to amend this privacy statement, and such changes will be effective upon publication.</p>
-								<p>&nbsp;</p>
-								<p><strong>How we collect information</strong></p>
-								<p>In order to provide our services, we may collect or receive information from our publishers, supply-side platforms, ad networks, and other providers of mobile ad inventory information (“Clients”).  This information may include location data, IP address, context, gender, age, other demographic information, device data such as mobile identifiers, carrier network information, the mobile website or mobile app used, and cookie or pixel tag data.  We may also collect information about how consumers interact with the ads we deliver including <em>information such as but not limited to </em>click thru rates, secondary clicks on Start Tank-hosted landing pages, and device information. The ad requests we receive may consist of personally identifiable information (PII), non-personally identifiable information (NPII) or both.  While we may receive personal information, we do not attempt to identify the individual consumer the information is associated with.</p>
-								<p>&nbsp;</p>
-								<p><strong>How we work with our clients</strong></p>
-								<p>The information we receive from our publisher Clients and publishers via third-party clients enables Start Tank to deliver relevant advertisements to consumers via their computer, mobile, or other internet accessible devices. We use this information to determine the most appropriate and relevant campaign to enhance the user experience and drive engagement. The type and amount of information provided to Start Tank is determined solely by our publisher partners and is necessary to provide our services.</p>
-								<p>Clients must ensure that they are able to send information to Start Tank pursuant to their respective privacy policy or other applicable agreement. Furthermore, Clients must disclose within their privacy policy that it allows third parties to serve advertisements and that cookies or other tracking devices may be used.</p>
-								<p>&nbsp;</p>
-								<p><strong>How we share personal information</strong></p>
-								<p>We may share personal information with:</p>
-								<ul>
-								<li>Members of the PayPal-eBay Inc. corporate family to provide joint content, products and services (like registration, transactions and customer support), to help detect and prevent potentially illegal acts and violations of our policies, and to guide decisions about their products, services and communications. Members of our corporate family will use this information to send marketing communications only if a consumer has requested their services and opted-in to ongoing communications pertaining to the service.</li>
-								<li>Service providers under contract who help with parts of our business operations such as fraud prevention, bill collection, marketing and technology services. Our contracts dictate that these service providers only use information in connection with the services they perform for us and not for their own benefit.</li>
-								<li>Law enforcement, government officials, or other third parties when we are compelled to do so by a subpoena, court order or similar legal procedure, and believe the disclosure of personal information is necessary to prevent physical harm or financial loss, to report suspected illegal activity or to investigate violations of our User Agreement.</li>
-								
-								<li>Companies that we plan to merge with or are acquired by. (Should such a combination occur, we will require that the new combined entity follow this privacy policy with respect to the processing of personal information.)</li>
-								<li>We may share aggregate or anonymous data with our Clients to report on campaign metrics, revenue and other analytic reports.</li>
-								</ul>
-								<p><strong> </strong></p>
-								<p><strong>External Links</strong></p>
-								<p>This Privacy statement only applies to information received or collected by Start Tank in conjunction with the Start Tank Services.  Start Tank does not control the privacy practices of third parties. Consumers who click on an advertisement, visit a third party site or interact with third party content are subject to the privacy policies of those third parties, where applicable.</p>
-								<p>&nbsp;</p>
-								<p><strong>Children´s Privacy</strong></p>
-								<p>Start Tank does not knowingly collect, store, or use information about children under the age of 13.  If we become aware of information in our system that relates to a child under 13, we will take commercially reasonable steps to delete that information.</p>
-								<p>&nbsp;</p>
-								<p><strong>How We Protect and Store Personal Information</strong></p>
-								<p>Throughout this policy, we use the term &#8220;personal information&#8221; to describe information that can be associated with a specific person and can be used to identify that person. We do not consider personal information to include information that has been anonymized so that it does not identify a specific user.</p>
-								<p>We store and process your personal information on our computers in the US and elsewhere in the world where our facilities are located.  We protect your information using technical and administrative security measures to reduce the risks of loss, misuse, unauthorized access, disclosure and alteration.  Some of the safeguards we use are firewalls and data encryption, physical access controls to our data centers, and information access authorization controls.</p>
-								<p>&nbsp;</p>
-								<p><strong>Contact Information</strong></p>
-								<p>Start Tank<br />
-								1 International Place</p>
-								<p>Boston, MA 02110</p>
-								<p>Phone: (617) 502-3100</p>
-								<p>Email: <strong> advertising@paypal.com</strong></p>
-								<p>This Privacy Statement was last updated September 5 2012</p>
-								
+<p>When you visit the website starttank.com, we collect information sent to us by your computer, mobile phone or other access device. The information sent to us includes data on the pages you access, your computer IP address, device identifiers, and the type of operating system you’re using, your location, mobile network information, standard web log data and other information. Web log data includes the browser type you’re using and traffic to and from our site. </p><p><a href="#wrapper">Back to top</a></p>
+
+<h2 id="two">How We Use Cookies and Similar Technologies</h2>
+
+<p>When you access our website we may place small data files on your computer or other device. These data files may be cookies, pixel tags, "Flash cookies," or other local storage provided by your browser or associated applications ("Cookies"). We use these technologies to: recognize you; customize Start Tank Services, content; and to promote trust and safety across our sites.
+</p><p>We use both session and persistent Cookies. Session Cookies expire and no longer have any effect when you log out of your account or close your browser. Persistent Cookies remain on your device until you erase them or they expire.
+</p><p>We encode our Cookies so that we can interpret the information stored in them. You are free to decline our Cookies if your browser or browser add-on permits, but doing so may interfere with your use of our website and Start Tank Services. Refer to the help section of your browser, browser extensions, or installed applications for instructions on blocking, deleting, or disabling Cookies.
+</p><p>You may encounter Start Tank Cookies on websites that we do not control. For example, if you view a web page created by a third party or use an application developed by a third party, there may be a Cookie placed by the web page or application. Likewise, these third parties may place their own Cookies that are not subject to our control and the Start Tank User Privacy Notice does not cover their use.
+</p><p>Do Not Track:  California law requires us to say how we respond to Do Not Track (DNT) signals. Because there is currently no industry or legal standard for recognizing or honoring DNT, we do not currently respond to DNT signals. We await the result of work by the privacy community and industry to determine when such a response is appropriate and what form it should take.  </p><p><a href="#wrapper">Back to top</a> 
+</p>
+
+
+<h2 id="three">How We Protect and Store Personal Information </h2>
+<p>Throughout this policy, we use the term "personal information" to describe information that can be associated with a specific person and can be used to identify that person. We do not consider personal information to include information that has been made anonymous so that it does not identify a specific user.
+</p><p>We store and process your personal information on our computers in the US and elsewhere in the world where our facilities are located. We protect your information using physical, technical, and administrative security measures to reduce the risks of loss, misuse, unauthorized access, disclosure and alteration. Some of the safeguards we use are firewalls and data encryption, physical access controls to our data centers, and information access authorization controls.</p><p><a href="#wrapper">Back to top</a> 
+</p>
+
+<h2 id="four">How We Use the Personal Information We Collect </h2><p>Our primary purpose in collecting personal information is to provide you with a secure, smooth, efficient, and customized experience. We may use your personal information to:  prevent potentially prohibited or illegal activities, and enforce our Terms of Use; customize, measure, and improve Start Tank Services and the content, layout, and operation of our website and applications.  </p><p><a href="#wrapper">Back to top</a> 
+</p>
+
+<h2 id="five">Marketing </h2><p>We do not sell or rent your personal information to third parties for their marketing purposes without your explicit consent. We may combine your information with information we collect from other companies and use it to improve and personalize Start Tank Services and content. </p><p><a href="#wrapper">Back to top</a> 
+</p>
+
+<h2 id="six">How We Share Personal Information with Other Parties </h2><p>We may share your personal information with:</p>
+<ul>
+	<li>Service providers under contract who help with our business operations such as fraud prevention, bill collection, marketing and technology services. Our contracts dictate that these service providers only use your information in connection with the services they perform for us and not for their own benefit.</li><li>Companies that we plan to merge with or be acquired by. (Should such a combination occur, we will require that the new combined entity follow this User Privacy Notice with respect to your personal information. If your personal information could be used contrary to this policy, you will receive prior notice.)</li><li>Law enforcement, government officials, or other third parties pursuant to a subpoena, court order, or other legal process or requirement applicable to Start Tank or one of its affiliates; when we need to do so to comply with law; or when we believe, in our sole discretion, that the disclosure of personal information is necessary to prevent physical harm or financial loss, to report suspected illegal activity or to investigate violations of our Terms of Use.</li><li>Other third parties with your consent or direction to do so.</li></ul>
+	
+</p><p>Start Tank will not sell or rent any of your personal information to third parties for their marketing purposes and only shares your personal information with third parties as described in this policy.</p><p><a href="#wrapper">Back to top</a> 
+</p>
+
+<h2 id="seven">How You Can Access or Change Your Personal Information </h2><p>You can review and edit your personal information at any time by contacting advertising@paypal.com.</p><p><a href="#wrapper">Back to top</a> 
+</p>
+
+<h2 id="eight">How You Can Contact Us about Privacy Questions </h2><p>If you have questions or concerns regarding this policy, you should contact us in writing to us at:</p><p>PayPal - Start Tank<br />1 International Place, 6th floor<br />Boston, MA  02110<br/>Email:  advertising@paypal.com</p><p><a href="#wrapper">Back to top</a> 
+</p><p>						
 	</div>
 </div>
 </div><!-- /onecolumn -->
