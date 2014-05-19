95fd45403f57cb9cb7f08b61c1d78e1a9af71e31
David Delmar <ddelmar@where.com>
2014-05-19 17:23:40 -0400

    edits to brandathon

---
+++ brandathon/assets/j/brandathon.js
@@ -0,0 +1,42 @@
+/*
+JavaScript for the demo: Recreating the Nikebetterworld.com Parallax Demo
+Demo: Recreating the Nikebetterworld.com Parallax Demo
+Author: Ian Lunn
+Author URL: http://www.ianlunn.co.uk/
+Demo URL: http://www.ianlunn.co.uk/demos/recreate-nikebetterworld-parallax/
+Tutorial URL: http://www.ianlunn.co.uk/blog/code-tutorials/recreate-nikebetterworld-parallax/
+
+License: http://creativecommons.org/licenses/by-sa/3.0/ (Attribution Share Alike). Please attribute work to Ian Lunn simply by leaving these comments in the source code or if you'd prefer, place a link on your website to http://www.ianlunn.co.uk/.
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+*/
+
+$(document).ready(function() { //when the document is ready...
+
+if (($(document).width()) < 768){
+} else {
+	
+	//save selectors as variables to increase performance
+	var $window = $(window);
+	var windowHeight = $window.height(); //get the height of the window
+	
+	//apply the class "inview" to a section that is in the viewport
+	$(".brandathon_fullspread .bg1").bind("inview", function (event, visible) {
+			if (visible == true) {
+			$(this).addClass("inview");
+			} else {
+			$(this).removeClass("inview");
+			}
+		});
+	$(".end_community").bind("inview", function (event, visible) {
+			if (visible == true) {
+			$(this).addClass("inview");
+			} else {
+			$(this).removeClass("inview");
+			}
+		});
+	
+}
+});
